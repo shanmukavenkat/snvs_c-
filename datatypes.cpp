8cbbf6502d0fb8d16ad86aadb2410d5e8810f8b8
SNVSKOMAL <21bq1a4208@vvit.net>
2023-10-23 14:29:31 +0530

    datatypes about c++

---
+++ datatypes.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    double b;
+    float ab;
+    string abc;
+    long d;
+    long long e;
+    a = 123;
+    b = 123123121231;
+    ab = 123.123;
+    abc = "DEFENDEF";
+    d=36916843698368367878712357843697467436;
+    e=4674679713873476743684368731679346793693684736973876;
+    cout<<"this is an integer datatype"<<a<<endl;
+    cout<<"this is an double datatype"<<b<<endl;
+    cout<<"this is a float datatype "<<ab<<endl;
+    cout<<"this is a string datatype"<<abc<<endl;
+    cout<<"this is a long datatype "<<d<<endl;
+    cout<<"this is the long long datatype "<<e<<endl;
+
+    return 0;
+}
---
+++ just creates.cpp
@@ -0,0 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"my name is snvskomal";
+    return 0;
+}
